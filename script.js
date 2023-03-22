31b3b927678b4e00103dd2970061971d46aaa69f
Mario Haefs <MarioHaefs@t-online.de>
2023-03-22 12:51:53 +0100

    added single Card call + design

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let pokemons = []
+
 async function loadPokemon() {
     for (let i = 1; i < 152; i++) {
         let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
@@ -6,6 +8,7 @@ async function loadPokemon() {
 
         document.getElementById('pokemon-card').innerHTML += pokedexHTML(i, currentPokemon);
 
+        pokemons.push(currentPokemon);
         setBackgroundColor(currentPokemon);
     }
 }
@@ -13,9 +16,9 @@ async function loadPokemon() {
 
 function pokedexHTML(i, currentPokemon) {
     return /*html*/`
-    <div class="pokemon-card"> 
+    <div onclick="openCard(${i})" class="pokemon-card"> 
         <h3>${currentPokemon['name']}</h3>
-        <img onclick="openCard(currentPokemon, ${i})" class="pokemon-img" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}"></img>
+        <img class="pokemon-img" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}"></img>
         <span id="pokemon-type">${currentPokemon['types'][0]['type']['name']}</span>
         <span id="pokemon-id">Nr. ${currentPokemon['id']}</span>
     </div>
@@ -23,29 +26,59 @@ function pokedexHTML(i, currentPokemon) {
 }
 
 
-function openCard(currentPokemon) {
+function openCard(i) {
     let container = document.getElementById('card-container');
+    let currentPokemon = pokemons[i - 1];
 
-    container.innerHTML += openCardHTML(currentPokemon);
+    container.innerHTML = '';
+    container.innerHTML += openCardHTML(currentPokemon, i);
     document.getElementById('card-container').style.display = 'flex';
-    document.getElementById('single-card').style.pointerEvents = 'none';
-
+    setBackgroundColorSingleCard(currentPokemon);
 }
 
 
 function openCardHTML(currentPokemon) {
     return /*html*/`
-    <div class="pokemon-single-card" id="single-card">
+    <div class="pokemon-single-card" id="single-card" onclick="doNotClose(event)">
         <h3>${currentPokemon['name']}</h3>
         <img class="pokemon-img" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}"></img>
         <span id="pokemon-type">${currentPokemon['types'][0]['type']['name']}</span>
+        <div class="stats">
+            <table>
+                <tr>
+                    <td class="space-right">${currentPokemon['stats'][0]['stat']['name']} :</td><td>${currentPokemon['stats'][0]['base_stat']} </td>             
+                </tr>
+                <tr>
+                    <td class="space-right">${currentPokemon['stats'][1]['stat']['name']} :</td><td>${currentPokemon['stats'][1]['base_stat']} </td>             
+                </tr>
+                <tr>
+                    <td class="space-right">${currentPokemon['stats'][2]['stat']['name']} :</td><td>${currentPokemon['stats'][2]['base_stat']} </td>             
+                </tr>
+                <tr>
+                    <td class="space-right">${currentPokemon['stats'][3]['stat']['name']} :</td><td>${currentPokemon['stats'][3]['base_stat']} </td>             
+                </tr>
+                <tr>
+                    <td class="space-right">${currentPokemon['stats'][4]['stat']['name']} :</td><td>${currentPokemon['stats'][4]['base_stat']} </td>             
+                </tr>
+                <tr>
+                    <td class="space-right">${currentPokemon['stats'][5]['stat']['name']} :</td><td>${currentPokemon['stats'][5]['base_stat']} </td>             
+                </tr>
+            </table>
+        </div>
     </div>
     `;
 }
 
 
+function hideSingleCard() {
+    document.getElementById('card-container').style.display = 'none';
+    document.getElementById('pokemon-single-card').style.display = 'none';
+}
 
 
+function doNotClose(event) {
+    event.stopPropagation();
+}
 
 
 function setBackgroundColor(currentPokemon) {
@@ -102,4 +135,61 @@ function setBackgroundColor(currentPokemon) {
 
     let pokemonCard = document.querySelector('.pokemon-card:last-child');
     pokemonCard.style.backgroundColor = backgroundColor;
+}
+
+
+function setBackgroundColorSingleCard(currentPokemon) {
+    let type = currentPokemon['types'][0]['type']['name'];
+    let backgroundColor;
+    switch (type) {
+        case 'grass':
+            backgroundColor = 'green';
+            break;
+        case 'fire':
+            backgroundColor = 'red';
+            break;
+        case 'water':
+            backgroundColor = 'blue';
+            break;
+        case 'bug':
+            backgroundColor = 'rgb(134, 196, 42)'
+            break;
+        case 'poison':
+            backgroundColor = 'rgb(65, 1, 65)'
+            break;
+        case 'electric':
+            backgroundColor = 'rgba(255, 255, 0, 0.800)'
+            break;
+        case 'ground':
+            backgroundColor = '#997917'
+            break;
+        case 'fairy':
+            backgroundColor = 'rgb(250, 126, 147)'
+            break;
+        case 'fighting':
+            backgroundColor = 'orange'
+            break;
+        case 'psychic':
+            backgroundColor = 'purple'
+            break;
+        case 'rock':
+            backgroundColor = 'rgb(65, 65, 65)'
+            break;
+        case 'ghost':
+            backgroundColor = 'rgb(16, 12, 54)'
+            break;
+        case 'ice':
+            backgroundColor = 'rgb(109, 128, 236)'
+            break;
+        case 'dragon':
+            backgroundColor = 'rgb(248, 59, 59)'
+            break;
+
+        default:
+            backgroundColor = 'gray';
+            break;
+    }
+
+    let pokemonSingleCard = document.querySelector('.pokemon-single-card:last-child');
+    pokemonSingleCard.style.backgroundColor = backgroundColor;
 }
